2495965c98326c7c5a73cb1466f0061a501a140d
Jason Brady <jasonmbrady42@gmail.com>
2020-11-29 18:44:45 -0800

    FirstJava complete

---
+++ fundamentals/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args){
+        System.out.println("My name is Jason");
+        System.out.println("I am 41 years old");
+        System.out.println("My hometown is Spokane, WA");
+    }
+}
